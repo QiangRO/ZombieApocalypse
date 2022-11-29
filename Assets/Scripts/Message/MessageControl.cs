619b2fcaf2faefaa37b979563ccb200552577526
QiangRO <aroch22222@hotmail.com>
2022-11-29 04:12:35 -0400

    Update MessageControl.cs

--- Assets/Scripts/Message/MessageControl.cs
+++ Assets/Scripts/Message/MessageControl.cs
@@ -8,20 +8,27 @@ public class MessageControl : MonoBehaviour
     //Tiempo que se muestra el mensaje
     [SerializeField]
     int timeToMessage;
+
     //Textos
     [SerializeField]
     Text guiaText;
+
     [SerializeField]
     Text objetivoText;
+
     [SerializeField]
     Text dialogoBoyText;
+
     [SerializeField]
     Text dialogoGirlText;
+
     //Backgrounds
     [SerializeField]
     GameObject panelGuia;
+
     [SerializeField]
     GameObject panelObjetivo;
+
     [SerializeField]
     GameObject panelDialogo;
 
@@ -31,6 +38,7 @@ public class MessageControl : MonoBehaviour
         StartCoroutine(TimeToMessageObjetivo());
         StartCoroutine(TimeToMessageDialogo());
     }
+
     //****************************************************************************************************
     //Mensajes guias
     public void gameOverMessage()
@@ -39,23 +47,47 @@ public class MessageControl : MonoBehaviour
         panelGuia.SetActive(true);
         StartCoroutine(TimeToMessageGuia());
     }
+
     public void precaucionZombies()
     {
         guiaText.text = "Los zombies te pueden matar";
         panelGuia.SetActive(true);
         StartCoroutine(TimeToMessageGuia());
     }
+
+    public void missionComplete()
+    {
+        guiaText.text = "Objetivo completado!!";
+        panelGuia.SetActive(true);
+        StartCoroutine(TimeToMessageGuia());
+    }
+
     //****************************************************************************************************
     //Mensajes objetivos
-    public void findSchool()
+    public void findFireDepartament()
     {
-        objetivoText.text = "Objetivo: Debes encontrar la escuela";
+        objetivoText.text = "Objetivo: busca a la niña en la estacion de bomberos";
         //panelObjetivo.SetActive(true);
         StartCoroutine(TimeToMessageObjetivo());
     }
+
     public void findPoliceStation()
     {
-        objetivoText.text = "Objetivo: Debes encontrar la estación de policía";
+        objetivoText.text = "Objetivo: Busca a la niña en la estacion de policia";
+        //panelObjetivo.SetActive(true);
+        StartCoroutine(TimeToMessageObjetivo());
+    }
+
+    public void findSchool()
+    {
+        objetivoText.text = "Objetivo: Busca a la niña en la escuela";
+        //panelObjetivo.SetActive(true);
+        StartCoroutine(TimeToMessageObjetivo());
+    }
+
+    public void findRescueZone()
+    {
+        objetivoText.text = "Objetivo: Escapa de la ciudad";
         //panelObjetivo.SetActive(true);
         StartCoroutine(TimeToMessageObjetivo());
     }
@@ -63,46 +95,79 @@ public class MessageControl : MonoBehaviour
     //****************************************************************************************************
     //Mensaje dialogos
     //-------PLAYER-------
-    public void mensajeEsperanza(){
+    public void mensajeEsperanza()
+    {
         dialogoBoyText.text = "Espero poder encontrar la escuela antes que me devoren los zombies";
         panelDialogo.SetActive(true);
         StartCoroutine(TimeToMessageDialogo());
     }
-    public void mensajeSusto(){
+
+    public void mensajeSusto()
+    {
         dialogoBoyText.text = "Maldita sea estos zombies son demasiado rapido";
         panelDialogo.SetActive(true);
         StartCoroutine(TimeToMessageDialogo());
     }
-    public void mensajeMunicion(){
+
+    public void mensajeMunicion()
+    {
         dialogoBoyText.text = "Espero que encuentre munición para mi arma";
         panelDialogo.SetActive(true);
         StartCoroutine(TimeToMessageDialogo());
     }
-    public void mensajeCiudad(){
+
+    public void mensajeCiudad()
+    {
         dialogoBoyText.text = "Esta ciudad es demasiado grande";
         panelDialogo.SetActive(true);
         StartCoroutine(TimeToMessageDialogo());
     }
-    public void mensajeEncontrarHermana(){
+
+    public void mensajeEncontrarHermana()
+    {
         dialogoBoyText.text = "Tengo que encontrar a mi hermana, lo mas antes posible";
         panelDialogo.SetActive(true);
         StartCoroutine(TimeToMessageDialogo());
     }
+
     //-------GIRL-------
+    public void girlMiedo()
+    {
+        dialogoGirlText.text = "Ten cuidado papa!!";
+        panelDialogo.SetActive(true);
+        StartCoroutine(TimeToMessageDialogo());
+    }
+
+    public void girlNoCorras()
+    {
+        dialogoGirlText.text = "No corras tan rapido papa!!";
+        panelDialogo.SetActive(true);
+        StartCoroutine(TimeToMessageDialogo());
+    }
+
+    public void girlEscucha()
+    {
+        dialogoGirlText.text = "Creo haber oido un helicoptero!!";
+        panelDialogo.SetActive(true);
+        StartCoroutine(TimeToMessageDialogo());
+    }
 
     //****************************************************************************************************
     //TIEMPO PARA QUE DESAPAREZCA EL MENSAJE
-    IEnumerator TimeToMessageGuia(){
+    IEnumerator TimeToMessageGuia()
+    {
         yield return new WaitForSeconds(timeToMessage);
         panelGuia.SetActive(false);
     }
-    
-    IEnumerator TimeToMessageObjetivo(){
+
+    IEnumerator TimeToMessageObjetivo()
+    {
         yield return new WaitForSeconds(timeToMessage);
         //panelObjetivo.SetActive(false);
     }
 
-    IEnumerator TimeToMessageDialogo(){
+    IEnumerator TimeToMessageDialogo()
+    {
         yield return new WaitForSeconds(timeToMessage);
         panelDialogo.SetActive(false);
     }
